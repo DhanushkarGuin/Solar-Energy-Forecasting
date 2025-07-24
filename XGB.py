142923e84863b10e2a19231e48675fa2f0c9b6dc
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-07-24 22:28:32 +0530

    XGBoost tested

---
+++ XGB.py
@@ -0,0 +1,31 @@
+## Importing the modded dataset
+import pandas as pd
+dataset = pd.read_csv('dataset.csv')
+
+## Dropping Redundant Variables
+dataset = dataset.drop(columns=['DC_POWER','TOTAL_YIELD','DATE','TIME','YEAR','MINUTES','TOTAL MINUTES PASS'])
+
+## Spliting Data for train and test
+X = dataset.drop(columns=['DAILY_YIELD'])
+y = dataset['DAILY_YIELD']
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y, train_size=0.2,random_state=42)
+
+## Pipelining
+from xgboost import XGBRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('model',XGBRegressor(eta=0.04,max_depth=5))
+])
+
+pipeline.fit(X_train,y_train)
+y_pred = pipeline.predict(X_test)
+
+## Metrics Evaluation
+from sklearn.metrics import root_mean_squared_error,r2_score
+print('RMSE:', root_mean_squared_error(y_test,y_pred))
+print('R2:', r2_score(y_test,y_pred))
