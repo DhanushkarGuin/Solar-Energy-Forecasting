5d209275dba98f97d11c0072c21eb135b2870f6f
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-07-25 07:59:13 +0530

    different target was tested

--- XGB.py
+++ XGB.py
@@ -3,11 +3,11 @@ import pandas as pd
 dataset = pd.read_csv('dataset.csv')
 
 ## Dropping Redundant Variables
-dataset = dataset.drop(columns=['DC_POWER','TOTAL_YIELD','DATE','TIME','YEAR','MINUTES','TOTAL MINUTES PASS'])
+dataset = dataset.drop(columns=['DAILY_YIELD','TOTAL_YIELD','DATE','TIME','YEAR','MINUTES','TOTAL MINUTES PASS'])
 
 ## Spliting Data for train and test
-X = dataset.drop(columns=['DAILY_YIELD'])
-y = dataset['DAILY_YIELD']
+X = dataset.drop(columns=['DC_POWER'])
+y = dataset['DC_POWER']
 
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y, train_size=0.2,random_state=42)
@@ -27,5 +27,5 @@ y_pred = pipeline.predict(X_test)
 
 ## Metrics Evaluation
 from sklearn.metrics import root_mean_squared_error,r2_score
-print('RMSE:', root_mean_squared_error(y_test,y_pred)) # 1836
-print('R2:', r2_score(y_test,y_pred)) # 60.37
+print('RMSE:', root_mean_squared_error(y_test,y_pred)) # 211
+print('R2:', r2_score(y_test,y_pred)) # 67.42%
