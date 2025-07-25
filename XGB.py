2565d3845f44b34694b74e2fb60b1b56b26e18e8
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-07-25 07:51:05 +0530

    Metrics updated

--- XGB.py
+++ XGB.py
@@ -27,5 +27,5 @@ y_pred = pipeline.predict(X_test)
 
 ## Metrics Evaluation
 from sklearn.metrics import root_mean_squared_error,r2_score
-print('RMSE:', root_mean_squared_error(y_test,y_pred))
-print('R2:', r2_score(y_test,y_pred))
+print('RMSE:', root_mean_squared_error(y_test,y_pred)) # 1836
+print('R2:', r2_score(y_test,y_pred)) # 60.37
